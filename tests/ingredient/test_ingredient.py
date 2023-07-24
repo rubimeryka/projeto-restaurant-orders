451cb56a298d689efce0d50e97f6160175da1fd5
rubimeryka <erykarubim13@gmail.com>
2023-07-24 17:19:56 -0300

    Requisito 01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1 commit
 def test_ingredient():
-    pass
+    ingrediente_um = Ingredient('acucar')
+    ingrediente_dois = Ingredient('ovo')
+    ingrediente_tres = Ingredient('farinha')
+
+    assert ingrediente_tres != ingrediente_dois
+    assert ingrediente_tres == ingrediente_tres
+
+    assert hash(ingrediente_dois) != hash(ingrediente_tres)
+    assert hash(ingrediente_dois) != hash(ingrediente_um)
+    assert hash(ingrediente_dois) == hash(ingrediente_dois)
+
+    assert repr(ingrediente_dois) == "Ingredient('ovo')"
+    assert ingrediente_dois.name == 'ovo'
+
+    assert ingrediente_tres.restrictions == {Restriction.GLUTEN}
